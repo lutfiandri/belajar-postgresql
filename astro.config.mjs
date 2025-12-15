a7144a45ce8a3fc4cef2a258e16f89c2a240f6e9
lutfiandri <lutfiandri232@gmail.com>
2025-12-15 15:01:32 +0700

    feat: setup awal, contoh soal 1 dan 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			plugins: [starlightThemeFlexoki()],
+			title: 'Belajar PostgreSQL',
+			customCss: ['./src/styles/custom.css'],
+			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
+					label: 'Leetcode SQL 50',
+					autogenerate: { directory: 'leetcode-sql-50' },
+				}
 			],
 		}),
 	],
