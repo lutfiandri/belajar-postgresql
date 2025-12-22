fd0cf53783785b6a284c75ae6d91f52533fa3079
lutfiandri <lutfiandri232@gmail.com>
2025-12-22 16:49:47 +0700

    feat: seo sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightThemeFlexoki from 'starlight-theme-flexoki';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeFlexoki from "starlight-theme-flexoki";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			plugins: [starlightThemeFlexoki()],
-			title: 'Belajar PostgreSQL',
-			customCss: ['./src/styles/custom.css'],
-			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'LeetCode SQL 50',
-					autogenerate: { directory: 'leetcode-sql-50' },
-				},
-				{
-					label: 'Basic SQL',
-					autogenerate: { directory: 'basic' },
-				},
-			],
-		}),
-	],
+  site: "https://belajar-postgresql.lutfiandri.my.id",
+  integrations: [
+    starlight({
+      plugins: [starlightThemeFlexoki()],
+      title: "Belajar PostgreSQL",
+
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/lutfiandri/belajar-postgresql",
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "LeetCode SQL 50",
+          autogenerate: { directory: "leetcode-sql-50" },
+        },
+        {
+          label: "Basic SQL",
+          autogenerate: { directory: "basic" },
+        },
+      ],
+    }),
+  ],
 });
